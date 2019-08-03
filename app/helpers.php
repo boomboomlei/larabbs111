0c2e94ac88b376512faa96f6e2d8a3cf0f4cb356
Your Nameboomboomlei <1846352100@qq.com>
2019-08-03 08:00:37 +0000

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -4,9 +4,14 @@
  * @Author: xuebingsi
  * @Date:   2019-07-31 09:26:30
  * @Last Modified by:   zhibinm
- * @Last Modified time: 2019-07-31 09:55:25
+ * @Last Modified time: 2019-08-03 15:55:46
  */
 function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
+}
+
+function category_nav_active($category_id)
+{
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
 }
